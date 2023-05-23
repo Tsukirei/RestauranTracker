c944062ae3720f65467373bb2c328e3f15dd1d86
Tsukirei <00049419@uca.edu.sv>
2023-05-23 10:07:13 -0600

    Add Model logic

---
+++ Moviles_Parcial/app/src/main/java/com/jchavez/restaurantracker/data/model/RestauranModel.kt
@@ -0,0 +1,8 @@
+package com.jchavez.restaurantracker.data.model
+
+data class RestauranModel(
+    var name:String,
+    var qualification:String,
+    var location:String,
+    var type:String
+    )
